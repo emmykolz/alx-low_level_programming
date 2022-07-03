423358ee54b9f63ec7c3f158b64f6c9c23392d3e
emmykolz <koladeee@gmail.com>
2022-07-02 17:10:48 -0700

    puts c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * This is my put funtion
+ * second line
+ */
+
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
