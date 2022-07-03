5a3743d633e984bdf3a8109473f73954d669cd16
emmykolz <koladeee@gmail.com>
2022-07-02 18:24:19 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is my fuction
+ * Return: 1
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora korpar, 2015-10-19\n");
+	return (1);
+}
