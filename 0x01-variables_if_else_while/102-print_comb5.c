ea9d93bced9db3e7506ce7f743fc2f1d9b91cf4e
emmykolz <koladeee@gmail.com>
2022-07-03 18:55:58 -0700

    comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Print all double digit numbers with comma and space
+ * Return: 0
+ */
+
+int main(void)
+{
+	int first;
+	int second;
+
+	for (first = 0; first <= 100; first++)
+	{
+		for (second = 0; second <= 100; second++)
+		{
+			if (second > first)
+			{
+				putchar(first / 10 + '0');
+				putchar(first % 10 + '0');
+				putchar(' ');
+				putchar(second / 10 + '0');
+				putchar(second % 10 + '0');
+				if (first * 100 + second != 9899)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
