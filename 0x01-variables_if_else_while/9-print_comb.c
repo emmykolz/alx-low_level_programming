83c24448e035a854fc132ab9d492b2d4c9934288
emmykolz <koladeee@gmail.com>
2022-07-03 17:32:54 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print all single digit numbers with comma and space
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 9; digit++)
+		putchar(digit % 10 + '0');
+		putchar(', ')
+	putchar(9 % 10 + '0');
+	putchar('\n');
+	return (0);
+}
