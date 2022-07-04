96d790d1349bd7937a2b5530bbf02d6ae950437a
emmykolz <koladeee@gmail.com>
2022-07-03 17:19:55 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,11 +7,10 @@
 int main(void)
 {
 	int digit;
+	char letter;
 
 	for (digit = 0; digit < 10; digit++)
 		putchar(digit % 10 + '0');
-	
-	char letter;
 
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);	}
