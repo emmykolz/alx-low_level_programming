3b6148812c879971b71879d828f1a76454a8b106
emmykolz <koladeee@gmail.com>
2022-07-03 17:35:07 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,8 +9,10 @@ int main(void)
 	int digit;
 
 	for (digit = 0; digit < 9; digit++)
+	{
 		putchar(digit % 10 + '0');
-		putchar(', ')
+		putchar(', ');
+	}
 	putchar(9 % 10 + '0');
 	putchar('\n');
 	return (0);
