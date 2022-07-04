0605b78a4c6e1c7ca85274befec3d65de4d92781
Emmanuel Kolade <koladeee@gmail.com>
2022-07-04 02:48:22 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,35 +6,34 @@
 
 int main(void)
 {
-	int first;
-	int second;
-	int third;
+int first;
+int second;
+int third;
 
-	for (first = 48; first < 58; first++)
-	{
-		for (second = 48; second < 58; second++)
-		{
-			if (second > first)
-			{
-				for (third = 48; third < 58; third++)
-				{
-					if (third > second)
-					{
-						putchar(first);
-						putchar(second);
-						putchar(third);
-						if (first == 55 && second == 56 && third == 57)
-						{
-						break;
-						}
-					putchar(',');
-					putchar(' ');
-					}
-				}
-			}
-		}
-	}
-
-	putchar('\n');
-	return (0);
+for (first = 48; first < 58; first++)
+{
+for (second = 48; second < 58; second++)
+{
+if (second > first)
+{
+for (third = 48; third < 58; third++)
+{
+if (third > second)
+{
+putchar(first);
+putchar(second);
+putchar(third);
+if (first == 55 && second == 56 && third == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
