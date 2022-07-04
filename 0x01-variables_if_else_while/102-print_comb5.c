bc88a7a17e15ebb8b19094c11c7c16bd3151423a
Emmanuel Kolade <koladeee@gmail.com>
2022-07-04 03:01:16 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,28 +6,28 @@
 
 int main(void)
 {
-	int first;
-	int second;
+int first;
+int second;
 
-	for (first = 0; first < 100; first++)
-	{
-		for (second = 0; second < 100; second++)
-		{
-			if (second > first)
-			{
-				putchar(first / 10 + '0');
-				putchar(first % 10 + '0');
-				putchar(' ');
-				putchar(second / 10 + '0');
-				putchar(second % 10 + '0');
-				if (first * 100 + second != 9899)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+for (first = 0; first < 100; first++)
+{
+for (second = 0; second < 100; second++)
+{
+if (second > first)
+{
+putchar(first / 10 + '0');
+putchar(first % 10 + '0');
+putchar(' ');
+putchar(second / 10 + '0');
+putchar(second % 10 + '0');
+if (first * 100 + second != 9899)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
