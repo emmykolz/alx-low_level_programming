2bcefe602647c679f270877ea7aaeec14f0b3319
emmykolz <koladeee@gmail.com>
2022-08-01 15:22:33 -0700

    dog header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - new data type containg name, age , amd owner
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: owner of dog
+ *
+ * Description: describes the above lol
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
