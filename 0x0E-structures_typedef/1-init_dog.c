a15a026729ea54715128fa88d7812295dd2bbab0
emmykolz <koladeee@gmail.com>
2022-08-01 15:43:25 -0700

    init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -16,7 +16,7 @@
  * Return: Always 0
  */
 
-void init_dog(struct dog *d char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
