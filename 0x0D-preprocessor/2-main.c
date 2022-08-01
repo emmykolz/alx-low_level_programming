6f2681774dd011ea3c4b0fd22305b7cc2c50fd9a
emmykolz <koladeee@gmail.com>
2022-08-01 11:29:04 -0700

    main code current file path

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - prints name of file it was compiled from
+ *
+ *
+ *
+ * Return: 0
+ *
+ **/
+
+int main(void)
+
+{
+	printf(__FILE__"\n");
+
+	return (0);
+}
