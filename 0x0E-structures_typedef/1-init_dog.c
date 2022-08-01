903f6e833159412c22955b08e5eed9557fcf672b
emmykolz <koladeee@gmail.com>
2022-08-01 15:29:27 -0700

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+
+
+/**
+ *
+ * init_dog - Points info from dog.h to main
+ *
+ * @d: For dereferencing
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ * Return: Always 0
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
