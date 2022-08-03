ee6743ec46b1330166df154aa5d2e675b6ae8b9b
emmykolz <koladeee@gmail.com>
2022-08-03 15:11:48 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,11 +5,11 @@
  * print_name - prints a name
  * @name: person's name
  * @f: pointer to a function
- * Return: void   
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (f != NULL)
-		f(name):
+		f(name);
 }
